dc7ee67910cc3e5158bb7514c101de13be8b6d99
esliesabu <ee24019733@biust.ac.bw>
2026-04-30 00:28:40 +0200

    Fix database paths and update to      PostgreSQL

--- backend/jwt.js
+++ backend/jwt.js
@@ -1,8 +1,8 @@
-const jwt = require('jasonwebtoken');
+const jwt = require('jsonwebtoken');
 
 const generateToken = (payload) => 
     jwt.sign(payload, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN || '7b',
+        expiresIn: process.env.JWT_EXPIRES_IN || '7d',
     });
     
 const verifyToken = (token) => 
