da1c5b98a1ec8cea78a328a75c79a78deb20d97f
esliesabu <ee24019733@biust.ac.bw>
2026-05-01 21:19:46 +0200

    database error fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "https://task-manager-12-saif.onrender.com"
+const API_BASE_URL = import.meta.env.VITE_API_URL || "https://localhost:5000";
 
 
 
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,1 +1,1 @@
-export const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:5000";
+export const API_BASE_URL = import.meta.env.VITE_API_URL || "https://task-manager-12-saif.onrender.com";
